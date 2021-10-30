756ff9bae7f7bdaf50ca71f1a49a1af611c11d9c
anixx <petrusevska.am@icloud.com>
2021-10-30 01:39:08 +0100

    Add Bookmark Service

--- Services/Services/IBookmarkService.cs
+++ Services/Services/IBookmarkService.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Services
+﻿namespace Services
 {
-    interface IBookmarkService
+    using Entity.DTOs;
+    using System.Collections.Generic;
+
+    public interface IBookmarkService
     {
+        IEnumerable<BookmarkDTO> GetBookmarks();
+        IEnumerable<BookmarkDTO> GetBookmark(int id);
+        BookmarkDTO CreateBookmark(BookmarkDTO bookmark);
+        void UpdateBookmark(BookmarkDTO bookmark);
+        void DeleteBookmark(BookmarkDTO bookmark);
+
+        BookmarkDTO GetCatrgoryById(int? categoryId);
+        IEnumerable<BookmarkDTO> GetByCategory(int categoryId, int id);
     }
 }
